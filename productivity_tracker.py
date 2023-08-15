aa67bb20ef3183fe88be8aed1c9dc334dee37cd7
ARFA79 <arfa79lg@gmail.com>
2023-08-15 20:50:57 +0330

    d13

--- productivity_tracker.py
+++ productivity_tracker.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 #file = open("note.txt",)
+# new idea !! connect the git api to app
+hours=[6,7,6,6,7,6,4.5,6,7,5,2,2,6]
 
-hours=[6,7,6,6]
-
-days=[1,2,3,4]
+days=[1,2,3,4,5,6,7,8,9,10,11,12,13]
 
 plt.plot(days ,hours)
 
